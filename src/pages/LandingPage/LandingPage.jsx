7673d75ae154fa897f05ea033831e9f0527f70d2
CallmeHenry <contact@henrynguyen.ca>
2024-06-07 18:34:42 -0700

    add animated background for landing

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,20 +1,22 @@
 import Spline from '@splinetool/react-spline';
+import { BackgroundBeams } from '../../components/ui/background-beams';
 export default function LandingPage() {
   return (
     <>
       <div className='flex flex-col h-dvh'>
-
+      
         <div id="hero" className='flex h-dvh bg-gradient-to-tr from-blue-500 to-cyan-300'>
 
 
-          <Spline scene="https://draft.spline.design/zSCLXeUIWAiwxQFj/scene.splinecode" />
+          <Spline scene="https://draft.spline.design/zSCLXeUIWAiwxQFj/scene.splinecode" className='z-10'/>
 
-          <div className="prose-2xl prose-headings:text-blue-50 prose-headings:mb-0 prose-p:text-blue-100 flex flex-col justify-center mr-24 ">
+          <div className="prose-2xl prose-headings:text-blue-50 prose-headings:mb-0 prose-p:text-blue-100 flex flex-col justify-center mr-24 z-10">
             <h1 >Manage your devices</h1>
             <p>A computer hardware toolkit that allows users to scan the serial number of a device and return hardware specifications.
               The specs are stored in a database where users are able to view and manage their devices.</p>
           </div>
         </div>
+        <BackgroundBeams/>
       </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,jsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
     require('daisyui'),
+    [addVariablesForColors],
   ],
 }
 
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
