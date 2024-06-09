65ec1bf5190623a8bfb8752c0bedf72eb8b88a01
CallmeHenry <contact@henrynguyen.ca>
2024-06-08 23:53:03 -0700

    add shadcn ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,21 +6,47 @@ const {
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx}"],
-  darkMode: "class",
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './src/**/*.{js,jsx}',
+  ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       boxShadow: {
         input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [
+    require("tailwindcss-animate"),
     require('@tailwindcss/typography'),
     require('daisyui'),
     [addVariablesForColors],
-  ],
+  ]
 }
 
 function addVariablesForColors({ addBase, theme }) {
